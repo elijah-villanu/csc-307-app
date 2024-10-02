c04cc58e01cdfbfd65b25d3391bb13f4a2c6a44b
elijah-villanu <elijahred9@outlook.com>
2024-10-01 23:02:12 -0700

    Table Inputs Added

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,26 +1,10 @@
-// src/MyApp.jsx
+// src/MyApp.jsx (Empty State)
 import React, {useState} from "react";
 import Table from "./table";
+import Form from "./Form"; //Needs to be after table
 
 function MyApp() {
-  const [characters, setCharacters] = useState([
-      {
-        name: "Charlie",
-        job: "Janitor"
-      },
-      {
-        name: "Mac",
-        job: "Bouncer"
-      },
-      {
-        name: "Dee",
-        job: "Aspring actress"
-      },
-      {
-        name: "Dennis",
-        job: "Bartender"
-      }
-  ]);  
+  const [characters, setCharacters] = useState([]);  
 
   function removeOneCharacter(index){
     const updated = characters.filter((character, i) => {
@@ -34,6 +18,7 @@ function MyApp() {
         characterData={characters}
         removeCharacter={removeOneCharacter}
       />
+      <Form />
     </div>
   );
 }
