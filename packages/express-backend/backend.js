3c3b9ea882cb6ad6d7b44868c0c06eefa074bd14
elijah-villanu <elijahred9@outlook.com>
2024-10-09 09:14:16 -0700

    My First Rest API (Backend Complete)

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,8 +1,16 @@
 // backend.js
 import express from "express"; //Is an ES module
-
 const app = express();
 const port = 8000;
+
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(
+    `Example app listening at http://localhost:${port}`
+  );
+});
+
 const users = {
     users_list: [
       {
@@ -33,22 +41,32 @@ const users = {
     ]
   };
 
-// Array Filtering
+
+
 const findUserByName = (name) => {
-  return users["users_list"].filter(
+  return users["users_list"].filter( // Array Filtering
     (user) => user["name"] === name
   );
 };
 
-//User get request, endpoint ?name=Mac represents request name
+const findUserByJob = (job) =>{
+  return name_result.filter( // Array Filtering
+    (user) => user["job"] === job
+  );
+};
+//User get request with given query string
 app.get("/users", (req, res) => {
   const name = req.query.name;
+  const job = req.query.job;
+ 
   if (name != undefined) {
-    let result = findUserByName(name);
-    result = { users_list: result };
-    res.send(result);
+    let name_result = findUserByName(name);
+    name_result =  { users_list: name_result };
+    // let job_result =findUserByJob(job);
+
+    res.send(name_result);
   } else {
-    res.send(users);
+    res.send();
   }
 }); 
 
@@ -56,7 +74,6 @@ app.get("/users", (req, res) => {
 //Search by unique id
 const findUserById = (id) =>
   users["users_list"].find((user) => user["id"] === id); //.find only finds first instance
-
 app.get("/users/:id", (req, res) => {
   const id = req.params["id"]; //or req.params.id
   let result = findUserById(id);
@@ -67,7 +84,37 @@ app.get("/users/:id", (req, res) => {
   }
 });
 
-app.use(express.json());
+
+//Add user to list (No persistance)
+//POST HTTP Request that checks input 
+const addUser = (user) => {
+  users["users_list"].push(user);
+  return user;
+};
+app.post("/users", (req, res) => {
+  const userToAdd = req.body;
+  addUser(userToAdd);
+  res.send();
+});
+
+
+const deleteUser = (id) => {
+   //id is unique, finds first instance
+  let found = findUserById(id)
+  const index = users["users_list"].indexOf(found)
+  users["users_list"].splice(index,1) //delete item from arrary
+};  
+app.delete("/users/:id",(req, res) => {
+  const id = req.params["id"];
+  deleteUser(id)
+  res.send()
+});
+
+
+
+ 
+
+
 
 app.get("/", (req, res) => {
   res.send("Testing Nodemon");
@@ -75,10 +122,4 @@ app.get("/", (req, res) => {
 
 app.get("/users", (req, res) => {
     res.send(users);
-});
-
-app.listen(port, () => {
-  console.log(
-    `Example app listening at http://localhost:${port}`
-  );
 });
