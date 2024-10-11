7b5d22f3d0a38c7544b7d23d2e78efb8cf90a451
elijah-villanu <elijahred9@outlook.com>
2024-10-10 21:34:50 -0700

    Added Post User Function

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -14,15 +14,32 @@ function MyApp() {
     }  
 
   function updateList(person) {
-    setCharacters([...characters, person]);
-    } 
+    postUser(person) //Contains the promise
+      .then(() => setCharacters([...characters, person]))
+      .catch((error) => {
+        console.log(error);
+      })
+  } 
   
   function fetchUsers(){
     const promise = fetch("http://localhost:8000/users");
     return promise
   }
 
-  //Runs when Promise Fullfilled Successfully 
+  function postUser(person){
+    const promise = fetch("Http://localhost:8000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",//tells server is a json object
+      },
+      body: JSON.stringify(person), //Serialize JSON to string to pass
+    });
+
+    return promise //Don't wait for POST, instead return promise
+  }
+
+  //Fetch Users from backend and load into state
+  //Runs when Promise Fullfilled Successfully  
   useEffect(() => {
     fetchUsers()
             .then((res) => res.json()) //Is a promise to return once decoded
@@ -30,6 +47,7 @@ function MyApp() {
             .catch((error) => { console.log(error); });
   },[]); //[] argument to ensure only runs at first mount (do not touch main data once in state)
   
+
   
   
   //Display
